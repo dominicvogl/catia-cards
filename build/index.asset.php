d7ff2332d885cb452287880697c88767c2b97eb5
Dominic Vogl <dominic.vogl@cat-ia.de>
2023-07-08 07:11:55 +0200

    build the block

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '125dbc491be51a6b2a67');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '27a23b38dc7801e4b8ee');
